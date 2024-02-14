35164c0ec7187c5fff5cf06e7517392eff22a6e8
teddy cubaka <teddycubaka96@gmail.com>
2024-02-14 14:31:09 +0100

    style : add index.php documentation

--- public/index.php
+++ public/index.php
@@ -2,24 +2,36 @@
 
 use Slim\Factory\AppFactory;
 
-include('../app/router.php');
-require __DIR__ . ('/../app/database.php');
-require __DIR__ . '/../vendor/autoload.php';
+// Import the router, database, and autoload files
+require '../app/router.php';
+require '../app/database.php';
+require '../vendor/autoload.php';
 
+// Create a new Slim app instance
 $app = AppFactory::create();
 
-if ($_ENV['ENVIRONMENT'] === 'DEV') $app->addErrorMiddleware(true, true, true);
+// Add error middleware if the environment is set to DEV
+if ($_ENV['ENVIRONMENT'] === 'DEV') {
+    $app->addErrorMiddleware(true, true, true);
+}
 
 try {
-    $conx = new DatabaseConnector();
-    $db = $conx->get_connection();
+    // Create a new instance of the DatabaseConnector class
+    $databaseConnector = new DatabaseConnector();
+
+    // Get the database connection
+    $db = $databaseConnector->get_connection();
 } catch (PDOException $e) {
+    // Handle any exceptions that occur during database connection
     echo $e->getMessage();
     exit(500);
 }
 
+// Create a new instance of the Router class and pass the Slim app and database connection
+$router = new Router($app, $db);
 
-$routes = new Router($app, $db);
-$routes->routes();
+// Call the routes() method to define the routes
+$router->routes();
 
+// Run the Slim app
 $app->run();
